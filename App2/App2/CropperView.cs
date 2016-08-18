3c720b522798b392996d1064a000bdf3aad0b316
roig <roig@sela.co.il>
2016-08-18 17:55:34 +0300

    Crop Image

---
+++ App2/App2/CropperView.cs
@@ -0,0 +1,76 @@
+﻿using CoreGraphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+
+namespace App2
+{
+    public sealed class CropperView : UIView
+    {
+        private CGPoint _origin;
+        private CGSize _cropSize;
+        
+        public CropperView()
+        {
+            _origin = new CGPoint(0, 100);
+            _cropSize = new CGSize(100, 100);
+
+            BackgroundColor = UIColor.Clear;
+            Opaque = false;
+            Alpha = 0.5f;
+        }
+
+        public CGPoint Origin
+        {
+            get
+            {
+                return _origin;
+            }
+
+            set
+            {
+                _origin = value;
+                SetNeedsDisplay();
+            }
+        }
+
+        public CGSize CropSize
+        {
+            get
+            {
+                return _cropSize;
+            }
+            set
+            {
+                _cropSize = value;
+                SetNeedsDisplay();
+            }
+        }
+
+        public CGRect CropRect => new CGRect(Origin, CropSize);
+
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+
+            using (var g = UIGraphics.GetCurrentContext())
+            {
+
+                g.SetFillColor(UIColor.Black.CGColor);
+                g.FillRect(rect);
+
+                g.SetBlendMode(CGBlendMode.Clear);
+                UIColor.Clear.SetColor();
+
+                var path = new CGPath();
+                path.AddRect(new CGRect(_origin, _cropSize));
+
+                g.AddPath(path);
+                g.DrawPath(CGPathDrawingMode.FillStroke);
+
+                //MyUILibraryPixel.addView(0, 60, MyUiUtils.basewidth, MyUiUtils.baseheight-160);
+            }
+        }
+    }
+}
---
+++ App2/App2/ViewController.cs
@@ -0,0 +1,134 @@
+﻿using CoreGraphics;
+using System;
+using System.Drawing;
+using UIKit;
+
+namespace App2
+{
+    public partial class ViewController : UIViewController
+    {
+        private UIPinchGestureRecognizer _pinch;
+        private UIPanGestureRecognizer _pan;
+        private CropperView cropperView;
+        private UIImageView _imageView;
+        private string imagePath = "Image/thul-DSC_0048.jpg";
+        private int _width = 400;
+        private int _height = 400;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            SetLayout();
+            
+        }
+
+        public void SetLayout()
+        {
+             var  image = ResizeImage(UIImage.FromFile(imagePath), _width, _height);
+            
+            CGRect frame = new RectangleF(0, 0, _width, _height);
+            _imageView = new UIImageView()
+            {
+                Frame = frame,
+                Image = image
+            };
+            cropperView = new CropperView() { Frame = _imageView.Frame };
+            SetPen();
+            View.AddSubviews(_imageView,cropperView);
+        }
+
+
+
+        partial void UIButton3_TouchUpInside(UIButton sender)
+        {
+            var cropCGImage = _imageView.Image.CGImage.WithImageInRect(cropperView.CropRect);
+            using (var croppedImage = UIImage.FromImage(cropCGImage))
+            {
+                _imageView.Image = croppedImage;
+                _imageView.Frame = cropperView.CropRect;
+                _imageView.Center = View.Center;
+                cropperView.Origin = new CGPoint(_imageView.Frame.Left, _imageView.Frame.Top);
+                cropperView.Hidden = true;
+            }
+        }
+
+
+        public void SetPen()
+        {
+            nfloat dx = 0;
+            nfloat dy = 0;
+            _pan = new UIPanGestureRecognizer(() =>
+            {
+                if ((_pan.State == UIGestureRecognizerState.Began || _pan.State == UIGestureRecognizerState.Changed) && (_pan.NumberOfTouches == 1))
+                {
+
+                    var p0 = _pan.LocationInView(View);
+
+                    if (dx == 0)
+                        dx = p0.X - cropperView.Origin.X;
+
+                    if (dy == 0)
+                        dy = p0.Y - cropperView.Origin.Y;
+
+                    var p1 = new CGPoint(p0.X - dx, p0.Y - dy);
+
+                    cropperView.Origin = p1;
+                }
+                else if (_pan.State == UIGestureRecognizerState.Ended)
+                {
+                    dx = 0;
+                    dy = 0;
+                }
+            });
+
+            cropperView.AddGestureRecognizer(_pan);
+
+            nfloat s0 = 1;
+            _pinch = new UIPinchGestureRecognizer(() =>
+            {
+                nfloat s = _pinch.Scale;
+                nfloat ds = (nfloat)Math.Abs(s - s0);
+                nfloat sf = 0;
+                const float rate = 0.5f;
+
+                if (s >= s0)
+                {
+                    sf = 1 + ds * rate;
+                }
+                else if (s < s0)
+                {
+                    sf = 1 - ds * rate;
+                }
+                s0 = s;
+
+                cropperView.CropSize = new CGSize(cropperView.CropSize.Width * sf, cropperView.CropSize.Height * sf);
+
+                if (_pinch.State == UIGestureRecognizerState.Ended)
+                {
+                    s0 = 1;
+                }
+            });
+            cropperView.AddGestureRecognizer(_pinch);
+
+        }
+
+        partial void UIButton7_TouchUpInside(UIButton sender)
+        {
+            SetLayout();
+        }
+
+        public static UIImage ResizeImage(UIImage sourceImage, float width, float height)
+        {
+            UIGraphics.BeginImageContext(new SizeF(width, height));
+            sourceImage.Draw(new RectangleF(0, 0, width, height));
+            var resultImage = UIGraphics.GetImageFromCurrentImageContext();
+            UIGraphics.EndImageContext();
+            return resultImage;
+        }
+    }
+}
